399cbf6c6cb62f8b88ca49e74ac77d71ed5ba75e
Dumpling1902 <rmiadamaris@gmail.com>
2025-01-10 08:57:21 -0600

    Creando el método atacar

--- poo_con_python.py
+++ poo_con_python.py
@@ -1,14 +1,43 @@
 class Personaje:
     #Atributos de la clase
-    nombre = 'Default'
-    fuerza = 0
-    inteligencia = 0
-    vida = 0
-    defensa = 0
+
+    def __init__(self,nombre,fuerza,inteligencia,vida,defensa):
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.vida = vida
+        self.defensa = defensa
+
+    def atributos(self):
+        print(self.nombre)
+        print("-Fuerza:", self.fuerza)
+        print("-Inteligencia:", self.inteligencia)
+        print("-Vida:", self.vida)
+        print("-Defensa:", self.defensa)
+    def subir_nivel(self,fuerza,inteligencia,defensa):
+        self.fuerza += fuerza
+        self.inteligencia += inteligencia
+        self.defensa += defensa
+    def esta_vivo(self):
+        return self.vida > 0
+    def morir(self):
+        self.vida = 0
+        print(self.nombre, "Ha muerto")
+    def dañar(self,enemigo):
+        return self.fuerza - enemigo.defensa   
+    def atacar(self,enemigo):
+        daño = self.daño(enemigo) 
+        enemigo.vida = enemigo.vida - daño
+        print(self.nombre, "Ha realizado", daño, "Punto de daño a", enemigo.nombre)
+        print("Vida de", enemigo.nombre, "es", enemigo.vida) 
 #Variable del constructor vacío de la clase
-mi_personaje = Personaje()
-#Modificando los valores de los atributos
-mi_personaje.nombre = "Liam"
-mi_personaje.fuerza = 20
+mi_personaje = Personaje("Liam", 70, 60, 80, 50)
+mi_enemigo = Personaje ("Michael", 60,90,100,40)
+print(mi_personaje.dañar(mi_enemigo))
+#print(mi_personaje.esta_vivo())
+mi_personaje.atributos()
+mi_personaje.atacar(mi_enemigo)
+mi_enemigo.atributos()
+
 print("El nombre del personaje es", mi_personaje.nombre)
 print("La fuerza del personaje es", mi_personaje.fuerza)
