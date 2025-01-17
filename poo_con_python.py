a6ed965cc89f9778bbae88dd8eba62d8400183a0
Dumpling1902 <rmiadamaris@gmail.com>
2025-01-17 08:51:41 -0600

    Problema celestial

--- poo_con_python.py
+++ poo_con_python.py
@@ -44,9 +44,63 @@ class Guerrero (Personaje):
         super().__init__(nombre,fuerza,inteligencia,vida,defensa)
         self.espada = espada
 
-hercules = Guerrero("Hércules", 80, 50, 100, 100, 5)
+    #Pedirle al usuario escoger un arma
+    def cambiar_arma(self):
+        opcion = int(input("Elige un arma: (1) Espada de plata, daño 10. (2) Espada de bronce, daño 8"))
+        if opcion == 1:
+            self.espada = 10
+        elif opcion == 2:
+            self.espada = 8
+        else:
+            print("Valor incorrecto")
+
+    #Sobreescirbir método
+    def atributos(self):
+       super().atributos()
+       print("-Espada:", self.espada)
+
+    #Sobreescirbir el cálculo de daño
+    def dañar(self, enemigo):
+        return self.fuerza * self.espada -enemigo.defensa
+
+#Creando clase Mago que hereda de su clase padre "Personaje"
+class Mago (Personaje):
+    #Sobreescribir el constructor 
+    def __init__(self,nombre,fuerza,inteligencia,vida,defensa,libro):
+        #Llamar a la clase padre
+        super().__init__(nombre,fuerza,inteligencia,vida,defensa)
+        self.libro = libro
+
+    #Sobreescirbir método
+    def atributos(self):
+       super().atributos()
+       print("-Libro:", self.libro)
+
+    #Sobreescirbir el cálculo de daño
+    def dañar(self, enemigo):
+        return self.inteligencia * self.libro -enemigo.defensa
+
+liam = Personaje ("Liam", 20, 15, 100, 10)
+hercules = Guerrero("Hércules", 20,15,100,10,5)
+michael = Mago ("Michael", 20,15,100,10,5)
+#Imprimir atributos antes del ataque
+liam.atributos()
 hercules.atributos()
-print(hercules.espada)
+michael.atributos()
+#Ataques
+liam.atacar(hercules)
+hercules.atacar(michael)
+michael.atacar(liam)
+#hercules.cambiar_arma()
+#hercules.atributos()
+#print (hercules.espada)
+
+#Imprimir atributos antes del ataque
+liam.atributos()
+hercules.atributos()
+michael.atributos()
+
+
 
 #Variable del constructor vacío de la clase
 #mi_personaje = Personaje("Liam", 70, 60, 80, 50)
